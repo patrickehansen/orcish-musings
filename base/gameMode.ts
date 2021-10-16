92a5efac4237155b0be6272ef9e6ce2d7f486411
Tiskahar <tiskahar@adiclan.com>
2021-10-16 13:45:02 -0500

    game mode default stats

--- base/gameMode.ts
+++ base/gameMode.ts
@@ -1,3 +1,40 @@
 export class GameMode {
-  public baseroll = '3d6'
+  public baseroll = '3d6';
+  public stats = [
+    {
+      abridged: 'END',
+      full: 'Endurance',
+      description: 'How hard you endure',
+    },
+    {
+      abridged: 'STR',
+      full: 'Strength',
+      description: 'How hard you strength',
+    },
+    {
+      abridged: 'AGI',
+      full: 'Agility',
+      description: 'How hard you agility',
+    },
+    {
+      abridged: 'RES',
+      full: 'Reasoning',
+      description: 'How hard you reason',
+    },
+    {
+      abridged: 'ALT',
+      full: 'Alertness',
+      description: 'How hard you alert',
+    },
+    {
+      abridged: 'MAG',
+      full: 'Magic Affinity',
+      description: 'How hard you magic',
+    },
+    {
+      abridged: 'MAR',
+      full: 'Martial Affinity',
+      description: 'How hard you mart',
+    },
+  ];
 }
